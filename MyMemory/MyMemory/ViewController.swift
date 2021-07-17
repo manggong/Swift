f9fa8f377031d72929fa7fb3532788e82eba0b2b
manggong <jihwank1995@gmail.com>
2021-07-17 18:42:56 +0900

    add view

---
+++ MyMemory/MyMemory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMemory
+//
+//  Created by 김지환 on 2021/07/17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
