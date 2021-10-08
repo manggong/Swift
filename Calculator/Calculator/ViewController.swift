704176d073cc20f143654f74c47f44c9c0e29beb
manggong <jihwank1995@gmail.com>
2021-10-08 20:47:32 +0900

    feat: add numberPressed func

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -38,6 +38,8 @@ class ViewController: UIViewController {
         zeroButton.setTitleColor(.black, for: .normal)
         zeroButton.backgroundColor = .white
         zeroButton.setTitle("0", for: .normal)
+        zeroButton.tag = 1
+        zeroButton.addTarget(self, action: #selector(numberPressed(_:)), for: .touchUpInside)
         holder.addSubview(zeroButton)
         
         for x in 0..<3 {
@@ -45,6 +47,8 @@ class ViewController: UIViewController {
             button1.setTitleColor(.black, for: .normal)
             button1.backgroundColor = .white
             button1.setTitle("\(x+1)", for: .normal)
+            button1.tag = x + 2
+            button1.addTarget(self, action: #selector(numberPressed(_:)), for: .touchUpInside)
             holder.addSubview(button1)
         }
         
@@ -53,6 +57,8 @@ class ViewController: UIViewController {
             button2.setTitleColor(.black, for: .normal)
             button2.backgroundColor = .white
             button2.setTitle("\(x+4)", for: .normal)
+            button2.tag = x + 5
+            button2.addTarget(self, action: #selector(numberPressed(_:)), for: .touchUpInside)
             holder.addSubview(button2)
         }
         
@@ -61,18 +67,20 @@ class ViewController: UIViewController {
             button3.setTitleColor(.black, for: .normal)
             button3.backgroundColor = .white
             button3.setTitle("\(x+7)", for: .normal)
+            button3.tag = x + 8
+            button3.addTarget(self, action: #selector(numberPressed(_:)), for: .touchUpInside)
             holder.addSubview(button3)
         }
         
-        let clearButton = UIButton(frame: CGRect(x: 0, y: holder.frame.size.height - (buttonSize * 5), width: view.frame.size.width, height: buttonSize))
+        let clearButton = UIButton(frame: CGRect(x: 0, y: holder.frame.size.height - (buttonSize * 5), width: view.frame.size.width - buttonSize, height: buttonSize))
         clearButton.setTitleColor(.black, for: .normal)
         clearButton.backgroundColor = .white
         clearButton.setTitle("clearAll", for: .normal)
         holder.addSubview(clearButton)
         
-        let operations = ["+", "-", "*", "/"]
+        let operations = ["=", "+", "-", "*", "/"]
         
-        for x in 0..<4 {
+        for x in 0..<5 {
             let button4 = UIButton(frame: CGRect(x: buttonSize * 3, y: holder.frame.size.height - (buttonSize * CGFloat(x+1)), width: buttonSize, height: buttonSize))
             button4.setTitleColor(.white, for: .normal)
             button4.backgroundColor = .orange
@@ -82,6 +90,23 @@ class ViewController: UIViewController {
         
         resultLabel.frame = CGRect(x: 20, y: clearButton.frame.origin.y - 100.0, width: view.frame.size.width - 40, height: 100)
         holder.addSubview(resultLabel)
+        
+        // Actions
+        clearButton.addTarget(self, action: #selector(clearResult), for: .touchUpInside)
+    }
+    
+    @objc func clearResult() {
+        resultLabel.text = "0"
+    }
+    
+    @objc func numberPressed(_ sender: UIButton) {
+        let tag = sender.tag - 1
+        
+        if resultLabel.text == "0" {
+            resultLabel.text = "\(tag)"
+        } else if let text = resultLabel.text {
+            resultLabel.text = "\(text)\(tag)"
+        }
     }
 }
 
