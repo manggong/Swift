3104e7fe879e68ad585496ae879b15ef4b0764ff
manggong <jihwank1995@gmail.com>
2021-10-18 20:14:56 +0900

    feat: Add tableView funcs

--- BountyList/BountyList/ViewController.swift
+++ BountyList/BountyList/ViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
+    
+    // 몇개?
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+    
+    // 보여줄 셀
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
+    }
+    
+    // 이벤트 액션
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("-->\(indexPath)")
+    }
 
 }
 
