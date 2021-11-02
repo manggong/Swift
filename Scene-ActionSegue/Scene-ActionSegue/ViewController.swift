6024d2a1fea836d9d245c7b402151b2333a36878
manggong <jihwank1995@gmail.com>
2021-11-02 19:52:12 +0900

    feat: Init project

---
+++ Scene-ActionSegue/Scene-ActionSegue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Scene-ActionSegue
+//
+//  Created by 김지환 on 2021/11/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
