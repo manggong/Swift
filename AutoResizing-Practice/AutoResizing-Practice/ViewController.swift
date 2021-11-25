cd2996d68e7995f76d499a817b1a29809267effe
manggong <jihwank1995@gmail.com>
2021-11-25 20:06:52 +0900

    test: Autolayout

---
+++ AutoResizing-Practice/AutoResizing-Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoResizing-Practice
+//
+//  Created by 김지환 on 2021/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
